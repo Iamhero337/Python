ba1b2850d0311b16ec083955f914fafe69f45712
Md Irfan <iamhero337@gmail.com>
2023-04-20 14:46:00 +0530

    Create 002.py

---
+++ Basic/002.py
@@ -0,0 +1,22 @@
+name = input("Name: ")
+age = int(input("Age(int): "))
+salary = int(input("Salary(int): "))
+
+print("Name:        ",name)
+print("Age:         ",age)
+print("Salary:      ",salary)
+print("Designation: ", end=" ")
+
+if salary < 10001:
+    print("Unknown")
+
+
+elif 10000 < salary <= 20000:
+    print("Intern")
+
+elif salary <= 35000:
+    print("Jr. SDE")
+
+else:
+    print("Product Manager")
+
