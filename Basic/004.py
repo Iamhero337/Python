157f7905a417b742fef22854d5f50d6508912e0f
Md Irfan <iamhero337@gmail.com>
2023-04-24 18:57:34 +0530

    update

--- Basic/004.py
+++ Basic/004.py
@@ -1,12 +1,13 @@
-year = int(input("Enter a year: "))
+year = int(input("Give a year: "))
 
 if year % 4 == 0:
     if year % 100 == 0:
         if year % 400 == 0:
-            print(f"{year} is a leap year")
+            print(f"{year} is a leap year.")
+
         else:
-            print(f"{year} is not a leap year")
+            print(f"{year} is not a leap year.")
     else:
-        print(f"{year} is a leap year")
+        print(f"{year} is a leap year.")
 else:
-    print(f"{year} is not a leap year")
+    print(f"{year} is a leap year.")
