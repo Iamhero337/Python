4d29b7d4dbba5f0816fbd983a34553a4bc5dfd8f
Md Irfan <iamhero337@gmail.com>
2023-04-25 19:54:03 +0530

    progress.

---
+++ Basic/006.py
@@ -0,0 +1,30 @@
+name = input("Name: ")
+roll = int(input("Roll: "))
+class_ = input("Class: ")
+eng = float(input("Marks in English: "))
+mat = float(input("Marks in Math: "))
+phy = float(input("Marks in Physics: "))
+bio = float(input("Marks in Bio: "))
+
+grade = (eng+mat+phy+bio)/4
+
+print(f"Roll: {roll}")
+print(f"Name: {name}")
+print(f"Class: {class_}")
+print("Percentage: ", grade, "%")
+print("Grade: ", end="")
+
+if grade >= 80:
+    print("A")
+
+elif grade >= 60:
+    print("B")
+
+elif grade >= 50:
+    print("C")
+
+elif    grade >= 45:
+    print("D")
+
+else:
+    print("F")
