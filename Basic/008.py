37132df632fcb324ab67969e93de1e8b88fb82eb
Md Irfan <iamhero337@gmail.com>
2023-04-28 23:49:29 +0530

    progress.

---
+++ Basic/008.py
@@ -0,0 +1,11 @@
+uAcount = 0
+lacount = 0
+
+
+char = input("Give the inputs (to stop press 'Enter'): ")
+
+uAcount = char.count('A')
+lacount = char.count('a')
+
+print("Number of Uppercase 'A' is: ", uAcount)
+print("Number of Lowercase 'a' is: ", lacount)
