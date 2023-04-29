53867f626fc474dc55212fe61d81d834959d9a8a
Md Irfan <iamhero337@gmail.com>
2023-04-29 20:45:22 +0530

    progress.

--- Basic/009.py
+++ Basic/009.py
@@ -1,1 +1,8 @@
-space = input("")
+space = input("Give the string to count the space in it: ")
+
+nofs = space.count(" ")
+
+if nofs <= 1:
+    print("No of space is ", nofs)
+else:
+    print("No of spaces is ",nofs)
