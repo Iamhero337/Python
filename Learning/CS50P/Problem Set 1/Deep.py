d24b12db7d1e3ec256e933be65b741ecc9816001
Md Irfan <iamhero337@gmail.com>
2023-10-14 20:38:44 +0530

    update

---
+++ Learning/CS50P/Problem Set 1/Deep.py
@@ -0,0 +1,7 @@
+a = input("What is the answer to the Great Question of Life, the Universe and Everything? ")
+
+if a in ["42", "forty-two", "forty two", "Forty two", "Forty Two"]:
+    print("Yes")
+    
+else:
+    print("No")
