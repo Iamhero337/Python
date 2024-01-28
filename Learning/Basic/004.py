232f1e702c5d153e56c693b0b9d1e8fce10f7891
iamhero337 <iamhero337@gmail.com>
2024-01-28 20:52:01 +0530

    The code was wrong

--- Learning/Basic/004.py
+++ Learning/Basic/004.py
@@ -1,13 +1,6 @@
 year = int(input("Give a year: "))
 
-if year % 4 == 0:
-    if year % 100 == 0:
-        if year % 400 == 0:
-            print(f"{year} is a leap year.")
-
-        else:
-            print(f"{year} is not a leap year.")
-    else:
-        print(f"{year} is a leap year.")
-else:
+if (year % 4 == 0) and ((year % 100 != 0) or (year % 400 == 0)):
     print(f"{year} is a leap year.")
+else:
+    print(f"{year} is not a leap year.")
