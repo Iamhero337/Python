23d34f42105986f7867f8ed6625a175ad517726a
iamhero337 <iamhero337@gmail.com>
2024-01-28 20:47:15 +0530

    Word error

--- Learning/Basic/003.py
+++ Learning/Basic/003.py
@@ -4,4 +4,4 @@ b = float(input("Give Breadth: "))
 if l == b:
     print("It is a Square, with the area of %.4f" %(l*b))
 else:
-    print("It is a Triangle, with the are of %.4f" %(l*b))
+    print("It is a Rectangle, with the are of %.4f" %(l*b))
