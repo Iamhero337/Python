a7ac05a763c5b0ea1c8deea15cdeb6203e3c9815
Md Irfan <iamhero337@gmail.com>
2024-10-27 10:38:49 +0530

    update

--- Learning/Basic/try.py
+++ Learning/Basic/try.py
@@ -1,11 +1,17 @@
+#
+#
+# #THIS IS JUST TRIAL CODE WHICH I DID AFTER VERY LONG TIME IN PYTHON SO THIS IS JUST A TEST.
+# khaana = input("khaana kaa naaam: ")
+# drink = input("which drink: ")
+# house = input("at whose house: ")
+#
+# print(f"Aaj maine khaana main {khaana} khaaya.")
+# print(f"Mujhe drink main {drink} mila")
+# print(f"aur yeh sab pata hai kiske ghar pe? huh {house} ke ghar pe")
+#
 
+msg = """What 
+the  heck is 
+happening"""
 
-#THIS IS JUST TRIAL CODE WHICH I DID AFTER VERY LONG TIME IN PYTHON SO THIS IS JUST A TEST.
-khaana = input("khaana kaa naaam: ")
-drink = input("which drink: ")
-house = input("at whose house: ")
-
-print(f"Aaj maine khaana main {khaana} khaaya.")
-print(f"Mujhe drink main {drink} mila")
-print(f"aur yeh sab pata hai kiske ghar pe? huh {house} ke ghar pe")
-
+print(msg)
