c36cf2c0695f3cf1180d4f5b09d4af17a0405efa
iamhero337 <iamhero337@gmail.com>
2025-02-06 11:11:54 +0530

    added more questions.

--- Pyclasses/49.py
+++ Pyclasses/49.py
@@ -1,2 +1,5 @@
+#49. WAP to count the number of occurance of a specified character in a given string.
+
 n = input("Give the input: ")
-val = input("Give the char to get to know the repetation of it in the earlier sentence ")
+val = input("Give the char to get to know the repetation of it in the earlier sentence: ")
+
